532e5430a60a8f531fd59bbb4d07e8ac190fdbf6
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-07-13 17:17:47 +0530

    'API key change'

--- src/API/YoutubeAPI.js
+++ src/API/YoutubeAPI.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 const  BaseUrl = 'https://yt-api.p.rapidapi.com'
 const apikey1 = process.env.REACT_APP_APIKEY1;
-// const apikey2 = process.env.REACT_APP_APIKEY2;
-// const apikey3 = process.env.REACT_APP_APIKEY3; 
 const options = {
   method: 'GET',
   headers: {
