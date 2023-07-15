13f1260acd1285caa545ae89b5e593734b234fe6
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-07-15 17:36:08 +0530

    changes

--- src/components/UpperNavbar.js
+++ src/components/UpperNavbar.js
@@ -1,20 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Context from '../context/Context'
 import { useLocation, useNavigate } from 'react-router-dom';
+
 const UpperNavbar = () => {
 
      const { setSearchcategory, theme } = useContext(Context);
      const [search, setSearch] = useState('');
      const Navigate = useNavigate();
      const location = useLocation()
-     useEffect(() => { }, [location])
+     useEffect(() => {},[location])
 
-     
 
-    const setCategory = (event)=>
-    {
-       setSearch(event.target.value)
-    }
+     const setCategory = (event) => {
+               setSearch(event.target.value)
+     }
 
      const searchResults = (event) => {
           if (search.length !== 0) {
@@ -42,12 +41,12 @@ const UpperNavbar = () => {
 
                          <li className='flex items-center justify-center'>
                               <div className='flex justify-center items-center'>
-                                   <input type="text" name='search' id='search' className='rounded-l-full cursor-text bg-slate-200 navbarSearch' placeholder='search' onKeyUp={searchEnter} onChange={setCategory} value={search} />
+                                   <input type="text" name='search' id='search' className='rounded-l-full cursor-text bg-slate-200 navbarSearch' placeholder='search' onKeyUp={searchEnter} onChange={setCategory} value={search}/>
                                    <div id='serchButton' className='flex justify-center items-center bg-slate-300 rounded-r-full cursor-pointer navbarSearchIcon' onClick={searchResults}><i className="fa-solid fa-magnifying-glass fa-lg"></i></div>
                                    <i className="fa-solid fa-microphone fa-lg cursor-pointer mx-3 hide" title='Speak to Search'></i>
                               </div>
                          </li>
-
+                         
                          <li className='flex items-center justify-center mx-3'>
                               <div className='flex justify-center items-center'>
                                    <i className="fa-solid fa-circle-plus fa-lg mx-2 cursor-pointer hide" title='Create'></i>
