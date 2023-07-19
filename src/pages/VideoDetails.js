155f543fa3ec3c524319683d134488f6ac8c9713
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-07-19 09:28:29 +0530

    changes

--- src/pages/VideoDetails.js
+++ src/pages/VideoDetails.js
@@ -25,7 +25,7 @@ const VideoDetails = () => {
     <div className='flex justify-center relative top-[60px] videoDetailpage'>
       <Video />
       <SuggestVideo />
-      <div className={`fixed right-[10px] bottom-[50px] cursor-pointer flex justify-center items-center rounded-full w-10 h-10 bg-${theme === 'light' ? 'white' : 'black'}`} onClick={moveTotop}>
+      <div className={`fixed right-[10px] bottom-[50px] cursor-pointer flex justify-center items-center rounded-full w-10 h-10 bg-${theme === 'light' ? 'white' : 'black'} text-${theme === 'light' ? 'black' : 'white'}`} onClick={moveTotop}>
           <i className="fa-solid fa-arrow-up fa-xl"></i>
         </div>
     </div>
