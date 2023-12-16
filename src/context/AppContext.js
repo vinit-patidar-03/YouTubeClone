36e903126fe5263fb7e1dab020a017d1a0e4b3c3
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-12-16 16:58:26 +0530

    "changes"

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,71 +1,64 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
-import  Context  from './Context'
+import Context from './Context'
 import { fetchData } from '../API/YoutubeAPI'
 
 const AppContext = (props) => {
 
-    const [loading,setLoading] = useState(true);
-    const [selectCategory,setSelectedCategory] = useState('Bhajans');
-    const [mainpageVideos,setMainPagevideos] = useState('');
-    const [searchResult,setSearchResults] = useState('');
-    const [searchcategory,setSearchcategory] = useState('Goldmines');
-    const [channel,setChannel] = useState('');
-    const [shortscategory,setShortsCategory] = useState('Bhajans');
-    const [theme,setTheme] = useState('light');
-    const [cid,setCid] = useState(':cid')
+  const [loading, setLoading] = useState(true);
+  const [selectCategory, setSelectedCategory] = useState('Bhajans');
+  const [mainpageVideos, setMainPagevideos] = useState('');
+  const [searchResult, setSearchResults] = useState('');
+  const [searchcategory, setSearchcategory] = useState('Goldmines');
+  const [channel, setChannel] = useState('');
+  const [shortscategory, setShortsCategory] = useState('Bhajans');
+  const [theme, setTheme] = useState('light');
+  const [cid, setCid] = useState(':cid')
 
-    
+  useEffect(() => {
+    fetchDataforSelectedCategory(selectCategory);
+    setShortsCategory(selectCategory);
+    themeChanger(theme);
+  }, [selectCategory, theme])
 
-    
-    useEffect(()=>
-    {
-        fetchDataforSelectedCategory(selectCategory);
-        setShortsCategory(selectCategory);
-        themeChanger(theme);
-    },[selectCategory,theme])
-
-    const themeChanger = (color)=>
-    {
-      if(theme === 'light')
-      {
-         document.body.style.backgroundColor = 'white';
-      }
-      else
-      {
-        document.body.style.backgroundColor = 'black';
-      }
+  const themeChanger = (color) => {
+    if (theme === 'light') {
+      document.body.style.backgroundColor = 'white';
     }
-   const fetchDataforSelectedCategory = ()=>
-   {
+    else {
+      document.body.style.backgroundColor = 'black';
+    }
+  }
+  const fetchDataforSelectedCategory = () => {
     setLoading(true);
-       fetchData(`search?query=${selectCategory}&geo=IN`).then((res)=>
-       {
-          setMainPagevideos(res.data.data);
-          setLoading(false);
-       })
-   }
+    fetchData(`search?query=${selectCategory}&geo=IN`).then((res) => {
+      setMainPagevideos(res.data.data);
+      setLoading(false);
+    })
+  }
 
-  return(
-    <Context.Provider value = {
-      {selectCategory,
-      setSelectedCategory,
-      mainpageVideos,
-      setMainPagevideos,
-      loading,
-      setLoading,
-      searchResult,
-      setSearchResults,
-      searchcategory,
-      setSearchcategory,
-      channel,
-      setChannel,
-      theme,
-      setTheme,
-      shortscategory,
-      setShortsCategory,
-      cid,setCid}
-       }>{props.children}</Context.Provider>
+  return (
+    <Context.Provider value={
+      {
+        selectCategory,
+        setSelectedCategory,
+        mainpageVideos,
+        setMainPagevideos,
+        loading,
+        setLoading,
+        searchResult,
+        setSearchResults,
+        searchcategory,
+        setSearchcategory,
+        channel,
+        setChannel,
+        theme,
+        setTheme,
+        shortscategory,
+        setShortsCategory,
+        cid, setCid
+      }
+    }>{props.children}</Context.Provider>
   )
 }
 
--- src/pages/PlaylistDetails.js
+++ src/pages/PlaylistDetails.js
@@ -7,7 +7,7 @@ import Context from '../context/Context';
 
 const PlaylistDetails = (props) => {
     const { pid } = useParams();
-     const {theme} = useContext(Context);
+    const { theme } = useContext(Context);
     const [channelPlaylistsVideos, setChannelPlaylistsVideos] = useState();
     useEffect(() => {
         fetchPlaylistVideos();
@@ -19,39 +19,40 @@ const PlaylistDetails = (props) => {
         })
     }
     return (
-        <><div className='mb-[50px]'>
-            <div className='flex justify-center'>
-                {channelPlaylistsVideos &&
-                    <div className={`mt-[80px] rounded-xl w-96 p-5 text-${theme ==='light'?'black':'white'}`} style={{backgroundColor:`${theme === 'light'?'#E5E5CB':'#E9E8E8'}`}}>
-                        <div className='w-full relative'>
-                            <img src={channelPlaylistsVideos.meta.thumbnail[3].url || channelPlaylistsVideos.meta.thumbnail[0].url} className='w-full' alt="thumbnail" />
-                            <div className='text-white text-center absolute right-2 bottom-2'>
-                                <img src="/images/playlist.png" className='w-7' alt="playlist" />
+        <>
+            <div className='mb-[50px]'>
+                <div className='flex justify-center'>
+                    {channelPlaylistsVideos &&
+                        <div className={`mt-[80px] rounded-lg w-96 p-5 text-${theme === 'light' ? 'black' : 'white'}`} style={{ backgroundColor: `${theme === 'light' ? '#E5E5CB' : 'gray'}` }}>
+                            <div className='w-full relative'>
+                                <img src={channelPlaylistsVideos.meta.thumbnail[3].url || channelPlaylistsVideos.meta.thumbnail[0].url} className='w-full' alt="thumbnail" />
+                                <div className='text-white text-center absolute right-2 bottom-2'>
+                                    <img src="/images/playlist.png" className='w-7' alt="playlist" />
+                                </div>
+                            </div>
+                            <div className='ml-3'>
+                                <h1 className='font-bold mt-2 text-lg'>{channelPlaylistsVideos.meta.title}</h1>
+                                <h3 className='font-semibold mt-2 text-sm'>{channelPlaylistsVideos.meta.channelTitle}</h3>
+                                <div className='flex mt-3'>
+                                    <h5 className='text-xs'>{channelPlaylistsVideos.meta.videoCountText}</h5>
+                                    <h5 className='text-xs ml-3'>{channelPlaylistsVideos.meta.viewCountText}</h5>
+                                    <h5 className='text-xs ml-3'>{channelPlaylistsVideos.meta.lastUpdated}</h5>
+                                </div>
                             </div>
                         </div>
-                        <div className='ml-3'>
-                            <h1 className='font-bold mt-2 text-lg'>{channelPlaylistsVideos.meta.title}</h1>
-                            <h3 className='font-semibold mt-2 text-sm'>{channelPlaylistsVideos.meta.channelTitle}</h3>
-                            <div className='flex mt-3'>
-                                <h5 className='text-xs'>{channelPlaylistsVideos.meta.videoCountText}</h5>
-                                <h5 className='text-xs ml-3'>{channelPlaylistsVideos.meta.viewCountText}</h5>
-                                <h5 className='text-xs ml-3'>{channelPlaylistsVideos.meta.lastUpdated}</h5>
-                            </div>
-                        </div>
-                    </div>
-                }
-            </div>
+                    }
+                </div>
 
-            <hr className='my-3' />
+                <hr className='my-3' />
 
-            <div className='flex flex-wrap justify-center'>
-                {channelPlaylistsVideos &&
-                    channelPlaylistsVideos.data.map((elem, index) => {
-                        return <PlaylistVideoCard video={elem} key={index} />
-                    })
-                }
+                <div className='flex flex-wrap justify-center'>
+                    {channelPlaylistsVideos &&
+                        channelPlaylistsVideos.data.map((elem, index) => {
+                            return <PlaylistVideoCard video={elem} key={index} />
+                        })
+                    }
+                </div>
             </div>
-        </div>
         </>
     )
 }
