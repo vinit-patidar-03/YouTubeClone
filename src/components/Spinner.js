c67a7cd9b0c5a0fabe5599fb535f71643dd23b59
Vinit Patidar <Vinitpatidar780@gmail.com>
2024-02-25 12:35:09 +0530

    "changes"

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -6,7 +6,7 @@ const Spinner = () => {
       <img
         src="/images/spinner.gif"
         alt="spinner"
-        className=" w-32  fixed top-[calc(60%-64px)] left-[calc(50%-48px)]"
+        className=" w-32  fixed top-[calc(50%-48px)] left-[calc(50%-64px)]"
       />
     </>
   );
