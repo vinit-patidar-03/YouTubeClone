f85db632a14b79b128b9e7d7549653e57da20bb0
Vinit Patidar <Vinitpatidar780@gmail.com>
2024-06-22 20:34:40 +0530

    .

--- src/API/YoutubeAPI.js
+++ src/API/YoutubeAPI.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 const BaseUrl = 'https://yt-api.p.rapidapi.com'
-const apikey2 = process.env.REACT_APP_APIKEY2;
+const apikey3 = process.env.REACT_APP_APIKEY3;
 // const apikey1 = process.env.REACT_APP_APIKEY1;
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': apikey2,
+    'X-RapidAPI-Key': apikey3,
     'X-RapidAPI-Host': 'yt-api.p.rapidapi.com'
   }
 };
